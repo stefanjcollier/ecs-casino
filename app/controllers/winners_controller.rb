dbfbd859e2361eb5400c23ca61301b355e498dcb
Stefan Collier <stefan.collier@electriccarscheme.com>
2024-12-05 23:19:14 +0000

    safe

--- app/controllers/winners_controller.rb
+++ app/controllers/winners_controller.rb
@@ -20,9 +20,9 @@ class WinnersController < ApplicationController
     @winner_2 = cash_outs.detect { _1.id == @winner_2_id }
     @winner_3 = cash_outs.detect { _1.id == @winner_3_id }
 
-    @winner_1.update(position: 1)
-    @winner_2.update(position: 2)
-    @winner_3.update(position: 3)
+    @winner_1&.update(position: 1)
+    @winner_2&.update(position: 2)
+    @winner_3&.update(position: 3)
     render :index
   end
 end
